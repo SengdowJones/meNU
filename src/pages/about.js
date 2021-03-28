c27b33a7123e9c88731f4ec4a1cf4d2562f6033f
Sengdao <sengdao.inthavong3@gmail.com>
2021-03-27 20:25:56 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -48,13 +48,12 @@ const About = () => {
       <div className='text-2xl text-center font-black'>
         <div class="relative mb-4 ">
             <label class="block ">
-                <label for="email" class="text-sm leading-7 text-gray-600">Macros</label>
+                <label for="email" class="text-sm leading-7 text-gray-600">Preferences</label>
                 <select
                     class="block w-full px-4 py-2 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0">
-                    <option>Corporate event</option>
-                    <option>Wedding</option>
-                    <option>Birthday</option>
-                    <option>Other</option>
+                    <option>Cal/Meal</option>
+                    <option>Slots</option>
+                    <option>Dining Hall</option>
                 </select>
             </label>
         </div>
