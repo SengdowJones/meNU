8c7ee3b93d47e8fccabdd8010a47c5be38ee1b55
Sengdao <sengdao.inthavong3@gmail.com>
2021-03-27 20:08:51 -0500

    Add dropdown menus

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,63 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className='grid grid-cols-4 gap-4 bg-white'>
-      <div className='text-2xl text-center font-black'>Diet</div>
-      <div className='text-2xl text-center font-black'>Allergies</div>
-      <div className='text-2xl text-center font-black'>Macros</div>
-      <div className='text-2xl text-center font-black'>Preferences</div>
+    <div className='grid grid-cols-4 gap-2 bg-white'>
+      <div className='text-2xl text-center font-black'>
+        <div class="relative mb-4 ">
+            <label class="block ">
+                <label for="email" class="text-sm leading-7 text-gray-600">Macros</label>
+                <select
+                    class="block w-full px-4 py-2 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0">
+                    <option>Corporate event</option>
+                    <option>Wedding</option>
+                    <option>Birthday</option>
+                    <option>Other</option>
+                </select>
+            </label>
+        </div>
+      </div>
+      <div className='text-2xl text-center font-black'>
+        <div class="relative mb-4 ">
+            <label class="block ">
+                <label for="email" class="text-sm leading-7 text-gray-600">Macros</label>
+                <select
+                    class="block w-full px-4 py-2 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0">
+                    <option>Corporate event</option>
+                    <option>Wedding</option>
+                    <option>Birthday</option>
+                    <option>Other</option>
+                </select>
+            </label>
+        </div>
+      </div>
+      <div className='text-2xl text-center font-black'>
+        <div class="relative mb-4 ">
+            <label class="block ">
+                <label for="email" class="text-sm leading-7 text-gray-600">Macros</label>
+                <select
+                    class="block w-full px-4 py-2 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0">
+                    <option>Corporate event</option>
+                    <option>Wedding</option>
+                    <option>Birthday</option>
+                    <option>Other</option>
+                </select>
+            </label>
+        </div>
+      </div>
+      <div className='text-2xl text-center font-black'>
+        <div class="relative mb-4 ">
+            <label class="block ">
+                <label for="email" class="text-sm leading-7 text-gray-600">Macros</label>
+                <select
+                    class="block w-full px-4 py-2 bg-gray-100 border-transparent rounded-lg focus:border-gray-500 focus:bg-white focus:ring-0">
+                    <option>Corporate event</option>
+                    <option>Wedding</option>
+                    <option>Birthday</option>
+                    <option>Other</option>
+                </select>
+            </label>
+        </div>
+      </div>
     </div>
   );
 };
